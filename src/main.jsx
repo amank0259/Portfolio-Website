db693d8b21068c6c6f20a9a63f57d6ff15bb6236
Aman kumar <amank0259@gmail.com>
2024-02-10 16:11:57 +0530

    fix errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import analytics from '../analytics.js'
-
-const app = createApp(App)
-app.use(analytics)
-app.mount('#app')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
