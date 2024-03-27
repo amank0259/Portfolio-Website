f934710b76bca78bb8deb659ed262c25286431d1
Aman kumar <amank0259@gmail.com>
2024-03-27 10:56:01 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
       <SocialLinks />
       <About />
       <Skills />
-      {/* <Projects /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -76,14 +76,14 @@ const Skills = () => {
     ]
 
     return (
-        <div name="skills" className='page-3 bg-gradient-to-b from-black to-gray-800 lg:pt-40 w-full h-screen pb-40 z-10'>
+        <div name="skills" className='page-3 bg-gradient-to-b from-black to-gray-800 lg:pt-40 w-full h-screen  pb-40 z-10'>
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white z-10'>
                 <div className='mt-10 lg:pt-20 pt-40'>
                     <h1 className='text-5xl font-extrabold border-b-4 border-gray-300 p-2 inline z-10'>Skills</h1>
                     <p className='py-6 mt-4 text-2xl font-bold z-10'>What I know...</p>
                 </div>
 
-                <div className='box-cont w-full grid grid-cols-2 sm:grid-cols-3 gap-10 text-center py-8 px-12 sm:px-0 z-10'>
+                <div className='box-cont w-full grid grid-cols-2 sm:grid-cols-3 gap-10 text-center my-16 px-12 sm:px-0 z-10'>
 
                     {tech.map(({ id, src, style, title }) => (
                         <div key={id} className={`box shadow-md hover:scale-105 duration-500 h-[120%] justify-center flex flex-col bg-gray-800 bg-opacity-90 py-2 rounded-lg z-10 ${style}`}>
